08904ae3c3d7e3c4c8f68ffa04045f0612d86e31
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2023-12-30 08:25:12 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,3 +3,6 @@ import numpy as np
 import tensorflow
 import streamlit as st
 
+model = tensorflow.keras.models.load_model("MobileNet.h5", compile=False)
+labels = {0: 'Cat', 1: 'Dog'}
+st.title("Cat Dog Classification")
