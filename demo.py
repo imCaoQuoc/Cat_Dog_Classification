54faa490fd18eb7e3f7f3add52516522062d863f
NGUYỄN CAO QUỐC <caoquocworkandstudy@gmail.com>
2023-12-30 08:28:39 +0700

    Update demo.py

--- demo.py
+++ demo.py
@@ -3,6 +3,27 @@ import numpy as np
 import tensorflow
 import streamlit as st
 
+# Load pre-trained model
 model = tensorflow.keras.models.load_model("MobileNet.h5", compile=False)
 labels = {0: 'Cat', 1: 'Dog'}
 st.title("Cat Dog Classification")
+
+
+
+# Upload image through Streamlit
+uploaded_image = st.file_uploader("Choose a dog or cat image", type="jpg")
+
+if uploaded_image is not None:
+    st.image(uploaded_image, caption="Uploaded Image.", use_column_width=True)
+    st.write("")
+    st.write("Classifying...")
+
+    # Preprocess and predict
+    img_array = preprocess_image(uploaded_image)
+    predictions = model.predict(img_array)
+
+    # Display result
+    if predictions[0][0] > predictions[0][1]:
+        st.write("Prediction: Dog")
+    else:
+        st.write("Prediction: Cat")
